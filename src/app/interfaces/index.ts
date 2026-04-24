afd62ab39d2ef2788edc269a4eb4aabdbcac0613
Juan Delgado <dsikorp@gmail.com>
2026-04-24 11:39:58 -0500

    MovieService

---
+++ src/app/interfaces/index.ts
@@ -0,0 +1,15 @@
+
+
+///////////////////
+
+
+
+export interface QueryParams {
+  language?: string;
+  include_image_language?: string;
+  page?: number;
+  sort_by?: string;
+  // Usamos nombres de propiedades con strings para soportar los puntos de TMDB
+  'primary_release_date.gte'?: string;
+  'primary_release_date.lte'?: string;
+}
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,14 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { QueryParams } from '../interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MoviesService {
+
+  private apiKey = environment.apiKey;
+  private urlApi = environment.urlApi;
   
   constructor(private http: HttpClient) {}
 
+  private executeQuery<T>(endpoint: string, params: QueryParams = {}): Observable<T> {
+    console.log('Petición HTTP realizada');
+    return this.http.get<T>(`${this.urlApi}${endpoint}`, {
+      params: {
+        api_key: this.apiKey,
+        ...params,
+      }
+    })
+  }
+
   getFeature() {
-    return this.http.get('https://api.themoviedb.org/3/movie/now_playing?api_key=YOUR_API_KEY');
+    return this.executeQuery('/discover/movie', {
+      'primary_release_date.gte': '2014-09-15',
+      'primary_release_date.lte': '2014-10-22',
+      language: 'en',
+      include_image_language: 'en'
+    });
   }
 }
+
